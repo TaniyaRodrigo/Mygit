f1bd9185ff5980d71ef024c80d6ac72533a505b7
TaniyaRodrigo <torodrigo1995@gmail.com>
2020-05-18 17:09:35 +0530

    commit 2

--- src/mygit/Mygit.java
+++ src/mygit/Mygit.java
@@ -17,6 +17,8 @@ public class Mygit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello GIT");
+         System.out.println("Welcome All");
+
     }
     
 }
